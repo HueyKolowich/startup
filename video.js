c42a27005aa036932c65868cf73735c34179afa6
Huey <huey@kolowich.org>
2024-03-01 00:17:16 -0500

    Added videosdk live library and created meeting object

--- video.js
+++ video.js
@@ -1,3 +1,16 @@
-const thisParticipantVideoContainer = document.getElementById("#thisParticipantVideoContainer")
+const thisParticipantVideoContainer = document.getElementById("thisParticipantVideoContainer");
 
 let meeting = null;
+
+async function fetchAuthToken() {
+    //Just return a default auth token here
+}
+
+fetchAuthToken().then(() => {
+    meeting = window.VideoSDK.initMeeting({
+        meetingId: "9rnr-tf27-4suk",
+        name: "Huey's Org",
+        micEnabled: true,
+        webcamEnabled: true,
+    });
+});
